10e5d9c5476be77c492deb46893fa5eecc03d924
Craig <40456374@live.napier.ac.uk>
2021-12-05 17:45:41 +0000

    updates

--- index.js
+++ index.js
@@ -4,24 +4,13 @@ app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static("public"));
 app.use(express.static("graphe"));
 app.use(express.json({ limit: "1mb" }));
-// POST method route
-app.post("/api", (request, response) => {
-  console.log("I got a request");
-  console.log(request.body, "\n");
-  const data = request.body;
-  response.json({
-    status: "success",
-    latitude: data.lat,
-    longitude: data.lon,
-  });
-});
 
 const mysql = require("mysql");
 const fs = require("fs");
 var con = mysql.createConnection({
   host: "34.89.35.185",
-  user: "root",
-  password: "password",
+  user: "Craig",
+  password: "Password",
   database: "sensordata",
   ssl: {
     ca: fs.readFileSync(__dirname + "/certs/server-ca.pem"),
@@ -53,6 +42,6 @@ app.get("/soil", (request, response) => {
       // console.log("array 1: ", result);
       response.json({ result });
     }
-  );
-
+  );   
+});
 
